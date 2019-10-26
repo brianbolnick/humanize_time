dcda1f35b3023aca0f951f69223dfc38677a4b87
Brian Bolnick <brianbolnick@gmail.com>
2019-10-26 13:44:29 -0600

    add custom formatters

--- lib/humanize_time.ex
+++ lib/humanize_time.ex
@@ -9,25 +9,26 @@ defmodule HumanizeTime do
           minutes: integer(),
           seconds: integer()
         }
+
   @doc """
   Formatter for converting seconds to a human readable format
 
   ## Examples
       iex> HumanizeTime.format_seconds(23487)
-
       "6 hr 31 min"
   """
-  @spec format_seconds(integer() | float()) :: String.t()
-  def format_seconds(nil), do: ""
+  @spec format_seconds(integer() | float(), keyword()) :: String.t()
+  def format_seconds(seconds, opts \\ [])
+  def format_seconds(nil, _), do: ""
 
-  def format_seconds(seconds) when is_float(seconds) do
+  def format_seconds(seconds, opts) when is_float(seconds) do
     seconds
     |> Float.round()
     |> Kernel.trunc()
-    |> format_seconds()
+    |> format_seconds(opts)
   end
 
-  def format_seconds(seconds) do
+  def format_seconds(seconds, opts) do
     initial_acc = %{
       days: 0,
       hours: 0,
@@ -36,7 +37,7 @@ defmodule HumanizeTime do
     }
 
     calculate_times(initial_acc, seconds)
-    |> print_duration()
+    |> print_duration(opts[:formatters])
   end
 
   @spec calculate_times(duration_map(), integer()) :: duration_map()
@@ -68,28 +69,57 @@ defmodule HumanizeTime do
     end
   end
 
-  @spec print_duration(duration_map()) :: String.t()
-  defp print_duration(duration) do
+  @spec print_duration(duration_map(), duration_map() | nil) :: String.t()
+  defp print_duration(duration, formatters) do
     %{days: days, hours: hours, minutes: minutes, seconds: seconds} = duration
+
+    default_formatter = default_formatters()
+
+    formats =
+      case formatters do
+        nil -> default_formatter
+        user_formats -> user_formats
+      end
+
+    days_f = formats[:days] || default_formatter[:days]
+    hours_f = formats[:hours] || default_formatter[:hours]
+    minutes_f = formats[:minutes] || default_formatter[:minutes]
+    seconds_f = formats[:seconds] || default_formatter[:seconds]
 
     cond do
       days > 0 ->
-        day_string = print_days(days)
-        hour_string = print_hours(hours, minutes)
+        day_string = days_f.(days)
+
+        rounded_hours =
+          if minutes >= 30 do
+            hours + 1
+          else
+            hours
+          end
+
+        hour_string = hours_f.(rounded_hours)
         String.trim("#{day_string} #{hour_string}")
 
       hours > 0 ->
-        hour_string = print_hours(hours, 0)
-        minute_string = print_minutes(minutes, seconds)
+        hour_string = hours_f.(hours)
+
+        rounded_mins =
+          if seconds >= 30 do
+            minutes + 1
+          else
+            minutes
+          end
+
+        minute_string = minutes_f.(rounded_mins)
         String.trim("#{hour_string} #{minute_string}")
 
       minutes > 0 ->
-        minute_string = print_minutes(minutes, 0)
-        seconds_string = print_seconds(seconds)
+        minute_string = minutes_f.(minutes)
+        seconds_string = seconds_f.(seconds)
         String.trim("#{minute_string} #{seconds_string}")
 
       true ->
-        String.trim(print_seconds(seconds))
+        String.trim(seconds_f.(seconds))
     end
   end
 
@@ -98,17 +128,24 @@ defmodule HumanizeTime do
   defp print_days(1), do: "1 day"
   defp print_days(duration), do: "#{duration} days"
 
-  @spec print_hours(integer(), integer()) :: String.t()
-  defp print_hours(0, _), do: ""
-  defp print_hours(duration, minutes) when minutes >= 30, do: "#{duration + 1} hr"
-  defp print_hours(duration, _), do: "#{duration} hr"
+  @spec print_hours(integer()) :: String.t()
+  defp print_hours(0), do: ""
+  defp print_hours(duration), do: "#{duration} hr"
 
-  @spec print_minutes(integer(), integer()) :: String.t()
-  defp print_minutes(0, _), do: ""
-  defp print_minutes(duration, seconds) when seconds >= 30, do: "#{duration + 1} min"
-  defp print_minutes(duration, _), do: "#{duration} min"
+  @spec print_minutes(integer()) :: String.t()
+  defp print_minutes(0), do: ""
+  defp print_minutes(duration), do: "#{duration} min"
 
   @spec print_seconds(integer()) :: String.t()
   defp print_seconds(0), do: ""
   defp print_seconds(duration), do: "#{duration} sec"
+
+  defp default_formatters do
+    %{
+      days: fn day -> print_days(day) end,
+      hours: fn hour -> print_hours(hour) end,
+      minutes: fn minute -> print_minutes(minute) end,
+      seconds: fn second -> print_seconds(second) end
+    }
+  end
 end
